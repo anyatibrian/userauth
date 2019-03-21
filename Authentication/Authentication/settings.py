74d07cbc4538ac5535aaf7295818a0080cc1add7
Patrick Fitz <patrickfitz@PATRICKs-MacBook-Pro-2.local>
2019-03-21 18:29:03 +0300

    Configure admin email

--- Authentication/Authentication/settings.py
+++ Authentication/Authentication/settings.py
@@ -128,3 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER =  'dongokene@gmail.com'
+EMAIL_HOST_PASSWORD = 'doKene32'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
