c645ba1dce0c0cbafa1641ae859d0524f9db40b3
Patrick Fitz <patrickfitz@PATRICKs-MacBook-Pro-2.local>
2019-03-21 19:25:47 +0300

    edit admin info

--- Authentication/Authentication/settings.py
+++ Authentication/Authentication/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER =  'dongokene@gmail.com'
+EMAIL_HOST_USER =  'dongokene21@gmail.com'
 EMAIL_HOST_PASSWORD = 'doKene32'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
